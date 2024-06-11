37f79d3b730b550b88fdc8d39be57a3fc68b01fb
WallenskogAlfa <nicolas.wallenskog@alfalaval.com>
2024-06-11 15:24:25 +0200

    Opcua changes works

--- examples/client-example.py
+++ examples/client-example.py
@@ -1,11 +1,11 @@
 import asyncio
 import logging
-
+ 
 from asyncua import Client
-
+ 
 _logger = logging.getLogger(__name__)
-
-
+ 
+ 
 class SubHandler(object):
     """
     Subscription Handler. To receive events from server for a subscription
@@ -13,27 +13,28 @@ class SubHandler(object):
     Do not do expensive, slow or network operation there. Create another
     thread if you need to do such a thing
     """
-
+ 
     def datachange_notification(self, node, val, data):
         print("New data change event", node, val)
-
+ 
     def event_notification(self, event):
         print("New event", event)
-
-
+ 
+ 
 async def main():
-    url = "opc.tcp://localhost:4840/freeopcua/server/"
+    #url = "opc.tcp://localhost:4840/freeopcua/server/"
+    url = "opc.tcp://localhost:4840"
     async with Client(url=url) as client:
         _logger.info("Root node is: %r", client.nodes.root)
         _logger.info("Objects node is: %r", client.nodes.objects)
-
+ 
         # Node objects have methods to read and write node attributes as well as browse or populate address space
         _logger.info("Children of root are: %r", await client.nodes.root.get_children())
-
-        uri = "http://examples.freeopcua.github.io"
+ 
+        uri = "urn:B&R/pv/"
         idx = await client.get_namespace_index(uri)
-        _logger.info("index of our namespace is %s", idx)
-
+        #_logger.info("index of our namespace is %s", idx)
+ 
         # get a specific node knowing its node id
         #var = client.get_node(ua.NodeId(1002, 2))
         #var = client.get_node("ns=3;i=2002")
@@ -42,30 +43,31 @@ async def main():
         #await var.read_value() # get value of node as a python builtin
         #await var.write_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
         #await var.write_value(3.9) # set node value using implicit data type
-
+ 
         # Now getting a variable node using its browse path
-        myvar = await client.nodes.root.get_child("/Objects/2:MyObject/2:MyVariable")
-        obj = await client.nodes.root.get_child("Objects/2:MyObject")
+        #myvar = await client.nodes.root.get_child("/Objects/2:MyObject/2:MyVariable")
+        myvar = await client.nodes.root.get_child("0:Objects/4:PLC/6:Modules/6:::/6:ALIOT/6:UNS/6:ShippingCompany/6:id")
+        #obj = await client.nodes.root.get_child("Objects/2:MyObject")
         _logger.info("myvar is: %r", myvar)
-
+ 
         # subscribing to a variable node
         handler = SubHandler()
         sub = await client.create_subscription(10, handler)
         handle = await sub.subscribe_data_change(myvar)
         await asyncio.sleep(0.1)
-
+ 
         # we can also subscribe to events from server
         await sub.subscribe_events()
         # await sub.unsubscribe(handle)
         # await sub.delete()
-
+ 
         # calling a method on server
-        res = await obj.call_method("2:multiply", 3, "klk")
-        _logger.info("method result is: %r", res)
+        #res = await obj.call_method("2:multiply", 3, "klk")
+        #_logger.info("method result is: %r", res)
         while True:
             await asyncio.sleep(1)
-
-
+ 
+ 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
--- examples/client-minimal.py
+++ examples/client-minimal.py
@@ -1,10 +1,50 @@
 import asyncio
-
+import sys
+import json
 from asyncua import Client
 
-url = "opc.tcp://localhost:4840/freeopcua/server/"
-namespace = "http://examples.freeopcua.github.io"
+url = "opc.tcp://127.0.0.1:4840"
+namespace = "urn:B&R/pv/"
 
+
+def createEntryList(valueTypeIdList, nodeList, nodeNameList):
+    """
+        Create list of dicts based on how the config file should look
+        with all the relevant data 
+        List of dicts is returned to write to json
+    """
+    writeList= []
+    translationDict = {3: "UInt16", 6: "Int32", 10: "Float", 12: "String"}
+    for i, typeId in enumerate(valueTypeIdList):
+        #skip 22 since it means its a structure and not a variable
+        if int(typeId) !=22: 
+            nodeInfo = dict({'NodeId': '', 'Name': '', 'Type': '', 'Unit': None, 'Message': None, 'EventType': 0, 'TagRuleSet': None})
+            nodeInfo["NodeId"] = nodeNameList[i]
+            writeList.append(nodeInfo)
+            writeList[i]["Name"] = nodeList[i]
+            writeList[i]["Type"] = translationDict[int(typeId)]
+    return writeList
+
+async def getLists(childrenOfNode):
+    """
+        Create relevant lists based on the children of the node 
+        list of DataValues are returned
+    """
+    async with Client(url=url):
+        valueTypeIdList = []
+        nameList = []
+        nodeNameList = []
+        for node in childrenOfNode:
+            valueType = await node.read_data_type()
+            valueTypeId = str(valueType).rsplit("=")[1]
+            valueTypeId = valueTypeId.rsplit(",")[0]
+            names = str(node).rsplit(".")[-1]
+            nodename = str(node)
+            nodeNameList.append(nodename)
+            nameList.append(names)
+            valueTypeIdList.append(valueTypeId)
+            
+        return valueTypeIdList, nameList, nodeNameList
 
 async def main():
 
@@ -14,16 +54,38 @@ async def main():
         nsidx = await client.get_namespace_index(namespace)
         print(f"Namespace Index for '{namespace}': {nsidx}")
 
-        # Get the variable node for read / write
-        var = await client.nodes.root.get_child(
-            f"0:Objects/{nsidx}:MyObject/{nsidx}:MyVariable"
-        )
-        value = await var.read_value()
-        print(f"Value of MyVariable ({var}): {value}")
+        nodesToGet = ["ns=6;s=::ALIOT:UNS.ShippingCompany",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[0]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[1]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[2]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[0].MachinePosition[0]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[0].MachinePosition[1]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[1].MachinePosition[0]",
+                      "ns=6;s=::ALIOT:UNS.ShippingCompany.Vessel.Section[0].Tank[2].MachinePosition[0]"
+                     ]
 
-        new_value = value - 50
-        print(f"Setting value of MyVariable to {new_value} ...")
-        await var.write_value(new_value)
+        filename= 'C:/Users/SELUNWAF/Documents/Scanjet_ict_1_0_0_ALIOT.json'
+        allEntrys = []
+
+        for i in range(len(nodesToGet)):
+            valueTypeIdList= []
+            nameList=[]
+            nodeNameList=[]
+            childrenOfNode = await  client.get_node(nodesToGet[i]).get_children()
+            valueTypeIdList, nameList, nodeNameList = await getLists(childrenOfNode)
+            entryList = createEntryList(valueTypeIdList, nameList, nodeNameList)
+            allEntrys.extend(entryList)
+        #write all entrys into the config file .json
+        jsonDict = {"DriverTagSet": allEntrys}
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(jsonDict, indent=2))
+
+        sys.exit()
+
+       #new_value = value - 50
+       #print(f"Setting value of MyVariable to {new_value} ...")
+       #await var.write_value(new_value)
 
         # Calling a method
         res = await client.nodes.objects.call_method(f"{nsidx}:ServerMethod", 5)
